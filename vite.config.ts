e717400220050970c1c71c02ef7e2d6b8e1b27d6
Syed Ahamed <zyedrazer.22@gmail.com>
2025-08-09 00:26:22 +0530

    fix: port fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: "0.0.0.0",
     port: 3001,
+    strictPort: true,
   },
   preview: {
+    host: "0.0.0.0",
     port: 3001,
+    strictPort: true,
   },
   optimizeDeps: {
     exclude: ["lucide-react"],
